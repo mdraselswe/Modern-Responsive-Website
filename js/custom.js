81000e8a0ca840e0d36063d7a391798d4cec41c4
Rasel141 <rrasel141@gmail.com>
2017-12-03 21:21:11 +0600

    add smooth scrolling easing plugin & complete navigation all part

--- js/custom.js
+++ js/custom.js
@@ -75,3 +75,39 @@ $(function () {
     });
 
 });
+
+/*=================================================
+                    NAVIGATION
+===================================================*/
+// show/hide transparent black navigation
+$(function () {
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() < 50) {
+            // hide nav
+            $("nav").removeClass("vesco-top-nav");
+            $("#back-to-top").fadeOut();
+        } else {
+            // show nav
+            $("nav").addClass("vesco-top-nav");
+            $("#back-to-top").fadeIn();
+        }
+    });
+
+});
+
+// Smooth scrolling
+$(function () {
+
+    $("a.smooth-scroll").click(function(event){
+        event.preventDefault();
+
+        // get/return id like #about, #work, #team and etc
+        var section = $(this).attr("href");
+
+        $("html, body").animate({
+            scrollTop: $(section).offset().top - 65
+        }, 1250, "easeInOutExpo");
+    });
+
+});
